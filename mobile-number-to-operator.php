254b74940da2f0c7252716f80b0decb97cc1cf03
Saksham Kushwaha <sakshamappinternational@gmail.com>
2017-02-09 17:44:46 +0530

    Create mobile-number-to-operator.php

---
+++ mobile-number-to-operator.php
@@ -0,0 +1,31 @@
+
+<?php
+
+$url="http://qualityemaillist.com/offers/?mobile=88588866611&email=susheel@sakshamapp.com&password=pass";
+
+$request_timeout = 60; // 60 seconds timeout
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url);
+
+curl_setopt($ch, CURLOPT_TIMEOUT, $request_timeout);
+
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $request_timeout);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$output = curl_exec($ch);
+
+$curl_error = curl_errno($ch);
+
+$getserver= curl_getinfo($ch);
+
+curl_close($ch);
+
+$array= json_decode($output, true);
+var_dump($array); 
+
+exit;
+
+?>
