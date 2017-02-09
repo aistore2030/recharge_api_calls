61a9b9fd194e7db30566747d557911b39528621f
Saksham Kushwaha <sakshamappinternational@gmail.com>
2017-02-09 17:42:05 +0530

    Create recharge-request.php

---
+++ recharge-request.php
@@ -0,0 +1,33 @@
+<?php
+
+
+$url="http://localhost:8080/httpapi/recharge-request?email=loginemail@id.com&api_key=k&recharge_operator=A&recharge_circle=10&recharge_number=9935457781&amount=10";
+
+$request_timeout = 60; // 60 seconds timeout
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url);
+
+curl_setopt($ch, CURLOPT_TIMEOUT, $request_timeout);
+
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $request_timeout);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$output = curl_exec($ch);
+
+$curl_error = curl_errno($ch);
+
+$getserver= curl_getinfo($ch);
+
+curl_close($ch);
+
+$array= json_decode($output, true);
+
+var_dump($array);
+
+
+exit;
+
+?>
